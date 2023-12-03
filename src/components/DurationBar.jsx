f31dfda19270a2351704542d6639fafd5c1d35f2
Nguyễn Hoàng Nhân <nguyen.hngnhan21@gmail.com>
2023-12-03 21:28:15 +0700

    implement play/pause

--- src/components/DurationBar.jsx
+++ src/components/DurationBar.jsx
@@ -29,22 +29,6 @@ const DurationBar = () => {
   const currentTime = useSelector((state) => state.music.currentTime); // Current time when play a song
   const isPlaying = useSelector((state) => state.music.isPlaying); // Check if the song is playing
 
-  const GetSongById = async (id) => {
-    try {
-      const response = await axios.get(
-        `${Base_URL}/songs/getSongById?songId=${id}`,
-        {
-          headers: {
-            Authorization: `Bearer ${access_token}`,
-          },
-        }
-      );
-      console.log(response.data);
-      return response.data;
-    } catch (error) {
-      // console.log("Error:", error);
-    }
-  };
   // // Get the max duration of the song
   const GetSongDuration = (audioRef) => {
     setDuration(audioRef.current.duration);
@@ -75,24 +59,24 @@ const DurationBar = () => {
   const [currentIndex, setCurrentIndex] = useState(0);
   // const [currentPartIndex, setCurrentPartIndex] = useState(0);
 
-  // const audioArray = [
-  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_1.mp3?alt=media&token=29b10cca-12b3-492f-a316-642e6d897396",
-  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_2.mp3?alt=media&token=edf54995-84e8-46a9-a342-8378fcdd76ae",
-  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_3.mp3?alt=media&token=74385b3f-3085-4f01-aad5-c741cfba4556",
-  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_4.mp3?alt=media&token=86bd1c56-83f2-46d0-b0f4-388ab395efd8",
-  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_5.mp3?alt=media&token=7ffb8f6c-f7ea-4e32-8c25-33b5c222c4ae",
-  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_6.mp3?alt=media&token=0ad2bd34-54b7-47b4-a2e7-ac34ba227e5d",
-  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_7.mp3?alt=media&token=9f48f901-69c5-4174-b16c-3e1bcdd43745",
-  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_8.mp3?alt=media&token=46e7f688-65f8-4463-829a-ece0e295741f",
-  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_9.mp3?alt=media&token=efe48b6d-68c7-4a97-b695-6e9f56ace32e",
-  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_10.mp3?alt=media&token=e39ad2bf-72c6-47c9-8b6c-38fe0ebad2e2",
-  // ];
-
   const audioArray = [
-    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2F50%20Feet%20-%20Somo%2F50%20Feet%20-%20Somo_1.mp3?alt=media&token=3b6bac15-c14b-43b0-9b51-257aa4a5582a",
-    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2F50%20Feet%20-%20Somo%2F50%20Feet%20-%20Somo_2.mp3?alt=media&token=c379a880-1478-4348-832e-c1b1790ae4dc",
-    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2F50%20Feet%20-%20Somo%2F50%20Feet%20-%20Somo_3.mp3?alt=media&token=40795bcb-f4e6-4321-aee2-7d7fa57cc6e2",
+    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_1.mp3?alt=media&token=29b10cca-12b3-492f-a316-642e6d897396",
+    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_2.mp3?alt=media&token=edf54995-84e8-46a9-a342-8378fcdd76ae",
+    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_3.mp3?alt=media&token=74385b3f-3085-4f01-aad5-c741cfba4556",
+    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_4.mp3?alt=media&token=86bd1c56-83f2-46d0-b0f4-388ab395efd8",
+    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_5.mp3?alt=media&token=7ffb8f6c-f7ea-4e32-8c25-33b5c222c4ae",
+    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_6.mp3?alt=media&token=0ad2bd34-54b7-47b4-a2e7-ac34ba227e5d",
+    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_7.mp3?alt=media&token=9f48f901-69c5-4174-b16c-3e1bcdd43745",
+    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_8.mp3?alt=media&token=46e7f688-65f8-4463-829a-ece0e295741f",
+    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_9.mp3?alt=media&token=efe48b6d-68c7-4a97-b695-6e9f56ace32e",
+    "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2Fbabe%20kajima%2Fbabe%20kajima_10.mp3?alt=media&token=e39ad2bf-72c6-47c9-8b6c-38fe0ebad2e2",
   ];
+
+  // const audioArray = [
+  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2F50%20Feet%20-%20Somo%2F50%20Feet%20-%20Somo_1.mp3?alt=media&token=3b6bac15-c14b-43b0-9b51-257aa4a5582a",
+  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2F50%20Feet%20-%20Somo%2F50%20Feet%20-%20Somo_2.mp3?alt=media&token=c379a880-1478-4348-832e-c1b1790ae4dc",
+  //   "https://firebasestorage.googleapis.com/v0/b/tunetown-6b63a.appspot.com/o/audios%2F50%20Feet%20-%20Somo%2F50%20Feet%20-%20Somo_3.mp3?alt=media&token=40795bcb-f4e6-4321-aee2-7d7fa57cc6e2",
+  // ];
   useEffect(() => {
     const initAudioContext = async () => {
       try {
@@ -115,23 +99,36 @@ const DurationBar = () => {
 
   useEffect(() => {
     console.log(sourceNode.current[currentIndex]);
-    if (!isAudioPlayed && sourceNode.current.length > 0) {
+    console.log("activate effect");
+    if (sourceNode.current.length > 0) {
+      console.log("Play", currentIndex);
       setIsAudioPlayed(true);
       sourceNode.current[currentIndex].start();
 
-      setTimeout(() => {
-        sourceNode.current[currentIndex + 1].start();
+      sourceNode.current[currentIndex].onended = () => {
+        sourceNode.current[currentIndex + 1].start(audioContext.currentTime);
         setCurrentIndex(currentIndex + 1);
-      }, (sourceNode.current[currentIndex].buffer.duration - 0.05) * 1000);
+      };
+
+      // setTimeout(() => {
+      //   sourceNode.current[currentIndex + 1].start();
+      //   setCurrentIndex(currentIndex + 1);
+      // }, (sourceNode.current[currentIndex].buffer.duration - 0.05) * 1000);
     }
   }, [isLoaded]);
 
   useEffect(() => {
     if (currentIndex + 1 < sourceNode.current.length) {
-      setTimeout(() => {
-        sourceNode.current[currentIndex + 1].start();
+      console.log(sourceNode.current[currentIndex]);
+      sourceNode.current[currentIndex].onended = () => {
+        sourceNode.current[currentIndex + 1].start(audioContext.currentTime);
         setCurrentIndex(currentIndex + 1);
-      }, (sourceNode.current[currentIndex].buffer.duration - 0.05) * 1000);
+      };
+
+      // setTimeout(() => {
+      //   sourceNode.current[currentIndex + 1].start();
+      //   setCurrentIndex(currentIndex + 1);
+      // }, (sourceNode.current[currentIndex].buffer.duration - 0.05) * 1000);
     }
   }, [currentIndex]);
 
@@ -168,6 +165,24 @@ const DurationBar = () => {
     }
   };
 
+  const playPauseAudio = async () => {
+    if (sourceNode.current.length == 0) {
+      loadAndPlayAudio();
+    } else {
+      if (isAudioPlayed) {
+        console.log("Stop", currentIndex, audioContext.currentTime);
+        audioContext.suspend();
+        setIsAudioPlayed(false);
+
+        setIsAudioPlayed(false);
+      } else {
+        console.log("Play", currentIndex);
+        audioContext.resume();
+        setIsAudioPlayed(true);
+      }
+    }
+  };
+
   // When click the play/pause button
   const handlePlayPause = () => {
     /**
@@ -194,7 +209,6 @@ const DurationBar = () => {
 
   // Update the currentTime every second
   useEffect(() => {
-    GetSongById(songObj.id);
     GetSongDuration(audioRef); // Get the duration of the song
     CheckPlaying(audioRef);
     let interval; // Count the isPlaying
@@ -271,7 +285,7 @@ const DurationBar = () => {
         </button>
 
         <div>
-          <button onClick={(e) => loadAndPlayAudio(0)}>Play</button>
+          <button onClick={(e) => playPauseAudio()}>Play</button>
         </div>
 
         {/* // Audio element */}
