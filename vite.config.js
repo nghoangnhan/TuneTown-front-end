77a3c3154d8e28e01e03ead5da67389d1fe8d253
Nguyễn Hoàng Nhân <nguyen.hngnhan21@gmail.com>
2024-05-10 14:06:05 +0700

    implement web security

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    headers: {
+      "x-frame-options": "deny",
+      "content-security-policy": JSON.stringify({
+        "default-src": ["'self'"],
+        "frame-ancestors": ["'self'"],
+        "form-action": ["http://localhost:4173"],
+      }),
+    },
+    cors: {
+      origin: ["http://localhost:8080", "http://localhost:3000"],
+    },
+  },
+});
