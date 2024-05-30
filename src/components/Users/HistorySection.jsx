420a1633829e81c99cd2a82b2ea560b42d29ca2f
Nguyễn Hoàng Nhân <nguyen.hngnhan21@gmail.com>
2024-05-30 14:12:28 +0700

    adjust history section

--- src/components/Users/HistorySection.jsx
+++ src/components/Users/HistorySection.jsx
@@ -11,6 +11,14 @@ const HistorySection = () => {
   const [songList, setSongList] = useState([]);
   const userId = localStorage.getItem("userId");
 
+  const formatDate = (dateListen) => {
+    const strDate = String(dateListen);
+    const year = strDate.substring(0, 4);
+    const month = strDate.substring(5, 7);
+    const day = strDate.substring(8, 10);
+    return `${day}/${month}/${year}`;
+  };
+
   const getListSongHistory = async () => {
     try {
       const response = await axios.post(
@@ -42,26 +50,18 @@ const HistorySection = () => {
     songName: songItem.song.songName,
     artists: songItem.song.artists.map((artist) => artist.userName + " "), // Assuming artists is an array
     genres: songItem.song.genres.map((genre) => genre.genreName + " "), // Assuming genres is an array
+    dateListen: formatDate(songItem.dateListen),
   }));
 
   const columnsSong = [
     {
-      title: "ID",
-      dataIndex: "id",
-      key: "id",
-      align: "center",
-      render: (text) => <a>{text}</a>,
-    },
-    {
-      title: "Poster", dataIndex: "poster", key: "poster",
+      title: "Poster",
+      dataIndex: "poster",
+      key: "poster",
       align: "center",
       render: (poster) => {
-        return (
-          <div className="flex items-center justify-center">
-            {poster}
-          </div>
-        );
-      }
+        return <div className="flex items-center justify-center">{poster}</div>;
+      },
     },
     {
       title: "Song Name",
@@ -82,22 +82,12 @@ const HistorySection = () => {
       key: "genres",
       align: "center",
     },
-
-    // {
-    //   title: "Action",
-    //   key: "action",
-    //   render: (_, record) => (
-    //     <Space size="middle">
-    //       {/* <button className="px-2 py-1 rounded-lg bg-slate-800">Edit</button> */}
-    //       {/* <button
-    //         className="py-1 px-2 bg-[#c42323e1] hover:bg-[#ea3f3f] text-white rounded-lg"
-    //         onClick={() => deleteSong(record.key)}
-    //       >
-    //         Delete
-    //       </button> */}
-    //     </Space>
-    //   ),
-    // },
+    {
+      title: "Listening Date",
+      dataIndex: "dateListen",
+      key: "dateListen",
+      align: "center",
+    },
   ];
 
   useEffect(() => {
@@ -107,7 +97,9 @@ const HistorySection = () => {
   if (!songList) return null;
   return (
     <div className="h-full min-h-screen xl:p-5 bg-backgroundPrimary dark:bg-backgroundDarkPrimary">
-      <div className="my-4 text-4xl font-bold text-primary dark:text-primaryDarkmode">History</div>
+      <div className="my-4 text-4xl font-bold text-primary dark:text-primaryDarkmode">
+        History
+      </div>
       <Table columns={columnsSong} dataSource={dataSongTable} />
     </div>
   );
