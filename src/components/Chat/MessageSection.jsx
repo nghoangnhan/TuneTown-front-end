bcc548add5a66133b9e6af21057c26e0d64df743
NNam2411 <nguyennam2002ro@gmail.com>
2024-06-03 12:18:19 +0700

    fix: fix chat time

--- src/components/Chat/MessageSection.jsx
+++ src/components/Chat/MessageSection.jsx
@@ -1,10 +1,9 @@
 import PropTypes from "prop-types";
 import { useEffect, useRef } from "react";
-import defaultAva from "../../assets/img/logo/logo.png";
-import useIconUtils from "../../utils/useIconUtils";
+import ChatItem from "./ChatItem";
 
 const MessageSection = ({ chatContent }) => {
-  const { Check, CheckSeen } = useIconUtils();
+
   const windownEndRef = useRef(null);
   const scollToBottom = (windownEndRef) => {
     windownEndRef.current?.scrollIntoView({ behavior: "smooth" });
@@ -14,59 +13,10 @@ const MessageSection = ({ chatContent }) => {
     scollToBottom(windownEndRef);
   }, [chatContent]);
   return (
-    <div className="flex flex-col w-full h-screen min-h-screen py-20 overflow-auto bg-backgroundChat dark:bg-backgroundChattingDark">
+    <div className="flex flex-col w-full h-screen min-h-screen py-20 overflow-auto bg-backgroundChat dark:bg-backgroundChattingDark" >
       {chatContent != null &&
         chatContent.map((chat, index) => (
-          <div
-            key={index}
-            className={`${chat.own == true && chat.type !== 2 ? "items-end" : (chat.own == false && chat.type !== 2 ? "items-start" : (chat.type == 2 ? "items-center"
-              : ""))} flex flex-col m-2 gap-2`}>
-            <div className="flex flex-row items-end ">
-
-              {/* Message */}
-              {/* 2 is SYSTEM, !2 is USERS */}
-              {chat.type !== 2 &&
-                <div className="flex flex-row items-center">
-                  {/* Avatar  */}
-                  <div className="w-8">
-                    {chat.own == false && chat.type !== 2 &&
-                      <img
-                        src={`${chat.sentUserAvatar ? chat.sentUserAvatar : defaultAva}`}
-                        alt="user"
-                        className="mt-2 bg-white rounded-full"
-                      />}
-                  </div>
-                  <div className="flex flex-col gap-1 mx-1">
-                    <h3 className="font-bold text-primary dark:text-primaryDarkmode">
-                      {chat.name ? chat.name : "Unknown"}
-                    </h3>
-                    <div className="flex flex-row items-end gap-1">
-                      <span className="max-w-xs px-2 py-1 text-base break-words border rounded-md bg-slate-200">
-                        {chat.message}
-                      </span>
-                      {chat.own && chat.seen === 1 && <CheckSeen className="ml-1 text-primary dark:text-primaryDarkmode" />}
-                      {chat.own && chat.seen === 0 && <Check className="ml-1 text-primary dark:text-primaryDarkmode" />}
-                    </div>
-                    <div className={`flex ${chat.own == true ? "justify-end" : "justify-start"}`}>
-                      <p className="text-xs text-slate-400">{chat.time}</p>
-                    </div>
-                  </div>
-                </div>}
-
-              {chat.type === 2 &&
-                <div className="flex flex-col items-center justify-center">
-                  <div className="flex flex-col items-center justify-center gap-2 mt-2">
-                    <h3 className="font-bold text-primary dark:text-primaryDarkmode">
-                      {chat.name ? chat.name : "SYSTEM MESSAGE"}
-                    </h3>
-                    <span className="max-w-xs px-2 py-1 text-base break-words rounded-md text-backgroundPlaylistDark dark:text-backgroundPlaylistHoverDark bg-backgroundComponentPrimary dark:bg-backgroundComponentPrimary">
-                      {chat.message}
-                    </span>
-                  </div>
-                </div>
-              }
-            </div>
-          </div>
+          <ChatItem key={index} index={index} chat={chat}></ChatItem>
         ))}
       <div ref={windownEndRef} />
     </div>
