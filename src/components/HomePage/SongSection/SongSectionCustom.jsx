cdfde95594d228b6947d7069849529d25e52d1a5
NNam2411 <nguyennam2002ro@gmail.com>
2024-06-20 21:12:00 +0700

    update

--- src/components/HomePage/SongSection/SongSectionCustom.jsx
+++ src/components/HomePage/SongSection/SongSectionCustom.jsx
@@ -2,23 +2,25 @@ import "../../../assets/CSS/ReactContexify.css";
 import { useEffect, useState } from "react";
 import SongItem from "../../Song/SongItem";
 import { useMusicAPIUtils } from "../../../utils/useMusicAPIUtils";
+import { useTranslation } from "react-i18next";
 
 // eslint-disable-next-line react/prop-types
 const SongSectionCustom = ({ titleSong, tryNew, listenAgain }) => {
     const { getListSongListenAgain, getListSongTryNew } = useMusicAPIUtils();
+    const { t } = useTranslation();
     const [songList, setSongList] = useState([]);
     // const [hasMoreSongs, setHasMoreSongs] = useState(false);
 
     useEffect(() => {
         if (listenAgain == true) {
             getListSongListenAgain().then((response) => {
-                setSongList(response.songs);
+                setSongList(response?.songs);
                 // console.log("songListAgain", songList);
             });
         }
         else if (tryNew == true) {
             getListSongTryNew().then((response) => {
-                setSongList(response.songs);
+                setSongList(response?.songs);
                 // console.log("songListTryNew", songList);
             })
         }
@@ -35,6 +37,7 @@ const SongSectionCustom = ({ titleSong, tryNew, listenAgain }) => {
                             <SongItem song={songItem} songOrder={index + 1} />
                         </div>
                     ))}
+                {songList.length === 0 && <div className="my-4 text-xl font-bold text-center text-primary dark:text-primaryDarkmode">{t("home.updating")}</div>}
             </div>
         </div>
     );
